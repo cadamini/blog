469f55e2d16ca82485c31bcb65ca36c993de95c4
cadamini <christian.adamini@invision.de>
2017-03-31 23:15:45 +0200

    Fix rubocop offenses in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,39 +1,34 @@
 class CommentsController < ApplicationController
-		before_filter :ensure_admin!, only: [:destroy]
-		
-	def new
-		@comment = Comment.new(post_id: params[:post_id])
-	end
+  before_filter :ensure_admin!, only: [:destroy]
 
-	def create
-		@post = Post.find (params[:post_id])
-  	@comment = @post.comments.create(comment_params)
-		if @comment.save
-			flash[:notice] = "Comment successfully created"
-			redirect_to post_path(params[:post_id])
-		else
-			flash[:error] = "Comment creation failed"
-			redirect_to post_path(params[:post_id])
-		end
-	end
+  def new
+    @comment = Comment.new(post_id: params[:post_id])
+  end
 
-	# creator and admin only
-	def destroy
-		if Comment.find(params[:id]).destroy
-			flash[:notice] = "Comment successfully deleted"
-		else 
-			flash[:error] = "Problem while deleting comment"
-		end
-		redirect_to post_path(params[:post_id])
-	end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      flash[:notice] = 'Comment successfully created'
+    else
+      flash[:error] = 'Comment creation failed'
+    end
+    redirect_to post_path(params[:post_id])
+  end
 
-	# creator and admin only
-	def edit
-	end
+  # creator and admin only
+  def destroy
+    if Comment.find(params[:id]).destroy
+      flash[:notice] = 'Comment successfully deleted'
+    else
+      flash[:error] = 'Problem while deleting comment'
+    end
+    redirect_to post_path(params[:post_id])
+  end
 
-	private
+  private
 
-	def comment_params
-		params.require(:comment).permit(:comment_id, :content)
-	end
+  def comment_params
+    params.require(:comment).permit(:comment_id, :content)
+  end
 end
