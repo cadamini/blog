6c937fdc8ea2cbfa0120b8c61da3fe50ad4a468a
cadamini <christian.adamini@invision.de>
2017-04-02 13:25:53 +0200

    Fix markdown rendering in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,19 @@
+require 'redcarpet'
+
 module ApplicationHelper
   def markdown(text)
     options = {
       filter_html:     true,
       hard_wrap:       true,
       escape_html: true,
-      link_attributes: { rel: 'nofollow', target: "_blank" },
+      link_attributes: { rel: 'nofollow', target: '_blank' },
       space_after_headers: true,
       fenced_code_blocks: true
     }
 
     extensions = {
       autolink:           true,
-      superscript:        true,
-      disable_indented_code_blocks: true
+      superscript:        true
     }
 
     renderer = Redcarpet::Render::HTML.new(options)
