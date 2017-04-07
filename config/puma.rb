20dbf4d8a068c32d1949ab97ad6bee6c9c0b9c8f
Christian Adamini <christian.adamini@invision.de>
2017-04-08 00:45:38 +0200

    Revert "Set puma workers 0 in development for windows"
    This reverts commit 239f85b240151b22e9c689d067abb469119a7021.

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,4 @@
-workers 0 # for windows development environment
-workers Integer(ENV['WEB_CONCURRENCY'] || 2) if Rails.env == 'production'
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
